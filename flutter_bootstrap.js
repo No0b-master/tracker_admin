1eeb5b1da278e067f6908196439d8c822ed0c0f0
Mayank Kumar Singh <mayank.singh3@escortskubota.com>
2024-10-23 11:06:11 +0530

    revert back

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1077434827"
+    serviceWorkerVersion: "3482432008"
   }
 });
